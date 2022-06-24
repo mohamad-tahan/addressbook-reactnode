19e6e77c0e9fa6781a042d5faf904e835f6e56e2
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-24 18:08:25 +0300

    Created routes for apis

--- node-addressbook/src/user/index.js
+++ node-addressbook/src/user/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { get, register, login } = require('./controller/user');
+const {addCont, getCont, removeContact, updateContact} = require('../contact/controller/contact')
 const router = Router();
 const testMiddleware = require('../../middleware/test');
 
@@ -9,6 +10,11 @@ const testMiddleware = require('../../middleware/test');
 router.get('/', testMiddleware(), (req, res) => get(req, res));
 router.post('/auth/register', register);
 router.post('/auth/login', login);
+router.post('/auth/addcontact', addCont)
+router.delete('/auth/removecontact', removeContact)
+router.put('/auth/updatecontact', updateContact)
+
+router.get('/getcontacts', testMiddleware(), (req, res) => getCont(req, res));
 
 
 module.exports = router;
