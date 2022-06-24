52a10b582d81a8b4e1fe4d9d3b853b5a151e44a8
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-24 18:05:39 +0300

    Created Contact Schema

--- node-addressbook/model/Contact.js
+++ node-addressbook/model/Contact.js
@@ -1,52 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
-    location: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-          },
-          coordinates: {
-            type: [Number],
-            required: true
-          }
-      },
-})
+  type: {
+    type: String,
+    enum: ["Point"],
+  },
+  coordinates: {
+    type: [Number],
+  },
+});
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     min: 3,
     max: 255,
   },
   phone: {
     type: String,
-    required: true,
+
     min: 3,
     max: 255,
-  },
-  relationship: {
-    type: String,
-    required: true,
-    min: 3,
-    max: 255,
-    unique: true,
   },
   email: {
     type: String,
-    required: true,
     min: 3,
     max: 1024,
   },
+
+  relationship: {
+    type: String,
+    min: 3,
+    max: 255,
+    unique:false,
+  },
+
   location: locationSchema,
-  
+
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
-  
+    ref: "User",
+  },
 });
 
-module.exports = mongoose.model('Contact', contactSchema);
+module.exports = mongoose.model("Contact", contactSchema);
